6ee1372906cb87754afc057745d019a75f492bf3
晃一 <kouichi@kouichi-no-MacBook-Air.local>
2018-12-29 00:47:40 +0900

    処理をクラス化

---
+++ CreateKnowledge.py
@@ -0,0 +1,99 @@
+import sys
+import os
+from lxml import html
+from glob import glob
+from collections import Counter
+
+import MeCab
+
+class CreateKnowledge():
+    def __init__(self, input_dir):
+        self.input_dir = input_dir
+
+    def Create(self):
+        tagger = MeCab.Tagger('')
+        tagger.parse('')  # parseToNode() の不具合を回避するために必要。
+        # 単語の頻度を格納するCounterオブジェクトを作成する。
+        # Counterクラスはdictを継承しており、値としてキーの出現回数を保持する。
+        frequency = Counter()
+        count_proccessed = 0
+
+        # glob()でワイルドカードにマッチするファイルのリストを取得し、マッチしたすべてのファイルを処理する。
+        for path in glob(os.path.join(self.input_dir, '*', 'wiki_*')):
+            print('Processing {0}...'.format(path), file=sys.stderr)
+
+            with open(path, encoding="utf-8") as file:  # ファイルを開く。
+                for title, content in self.IterDocs(file):  # ファイル内の全記事について反復処理する。
+                    if self.JudgePersonName(tagger, title):
+                        tokens = self.GetTokens(tagger, content)  # ページから名詞のリストを取得する。
+                        # Counterのupdate()メソッドにリストなどの反復可能オブジェクトを指定すると、
+                        # リストに含まれる値の出現回数を一度に増やせる。
+                        frequency = Counter()
+                        frequency.update(tokens)
+                        for token, count in frequency.most_common(5):
+                            print(token, count, end=" ")
+                        print('\n')
+
+                    # 10,000件ごとに進捗を表示。
+                    count_proccessed += 1
+                    if count_proccessed % 10000 == 0:
+                        print('{0} documents were processed.'.format(count_proccessed),
+                            file=sys.stderr)
+
+        # 全記事の処理が完了したら、上位30件の名詞と出現回数を表示する。
+        #for token, count in frequency.most_common(30):
+            #print(token, count)
+
+
+    def IterDocs(self, file):
+        """
+        ファイルオブジェクトを読み込んで、記事の中身（開始タグ <doc ...> と終了タグ </doc> の間のテキスト）を
+        順に返すジェネレーター関数。
+        """
+
+        for line in file:
+            if line.startswith('<doc '):
+                buffer = []  # 開始タグが見つかったらバッファを初期化する。
+                title = self.GetTitleFromHtml(line)
+            elif line.startswith('</doc>'):
+                # 終了タグが見つかったらバッファの中身を結合してyieldする。
+                content = ''.join(buffer)
+                yield title, content
+            else:
+                buffer.append(line)  # 開始タグ・終了タグ以外の行はバッファに追加する。
+
+    def GetTitleFromHtml(self, line):
+        tokens = html.fromstring(line)
+        token = tokens.attrib['title']
+        return token
+
+    def JudgePersonName(self, tagger, name):
+        node = tagger.parseToNode(name)
+        while node:
+            category1, category2, category3 = node.feature.split(',')[:3]
+            if category1 == '名詞' and category2 == '固有名詞' and category3 == '人名':
+                print("------------", name, "------------")
+                return True
+            node = node.next
+        return False
+
+    def GetTokens(self, tagger, content):
+        """
+        文書内に出現した名詞のリストを取得する関数。
+        """
+
+        tokens = []  # この記事で出現した名詞を格納するリスト。
+
+        node = tagger.parseToNode(content)
+        while node:
+            # node.featureはカンマで区切られた文字列なので、split()で分割して
+            # 最初の2項目をcategoryとsub_categoryに代入する。
+            category, sub_category = node.feature.split(',')[:2]
+            # 固有名詞または一般名詞の場合のみtokensに追加する。
+            if category == '名詞' and sub_category in ('固有名詞', '一般'):
+                #macの環境だとsurfaceで取れる値が単語ではなく文字列になるので修正
+                #tokens.append(node.surface)
+                tokens.append(node.feature.split(',')[6])
+            node = node.next
+
+        return tokens
--- word_frequency.py
+++ word_frequency.py
@@ -3,6 +3,7 @@ import os
 from lxml import html
 from glob import glob
 from collections import Counter
+from CreateKnowledge import CreateKnowledge
 
 import MeCab
 
@@ -14,90 +15,8 @@ def main():
 
     input_dir = sys.argv[1]  # コマンドラインの第1引数で、WikiExtractorの出力先のディレクトリを指定する。
 
-    tagger = MeCab.Tagger('')
-    tagger.parse('')  # parseToNode() の不具合を回避するために必要。
-    # 単語の頻度を格納するCounterオブジェクトを作成する。
-    # Counterクラスはdictを継承しており、値としてキーの出現回数を保持する。
-    frequency = Counter()
-    count_proccessed = 0
-
-    # glob()でワイルドカードにマッチするファイルのリストを取得し、マッチしたすべてのファイルを処理する。
-    for path in glob(os.path.join(input_dir, '*', 'wiki_*')):
-        print('Processing {0}...'.format(path), file=sys.stderr)
-
-        with open(path) as file:  # ファイルを開く。
-            for title, content in iter_docs(file):  # ファイル内の全記事について反復処理する。
-                if judge_person_name(tagger, title):
-                    tokens = get_tokens(tagger, content)  # ページから名詞のリストを取得する。
-                    # Counterのupdate()メソッドにリストなどの反復可能オブジェクトを指定すると、
-                    # リストに含まれる値の出現回数を一度に増やせる。
-                    frequency = Counter()
-                    frequency.update(tokens)
-                    for token, count in frequency.most_common(5):
-                        print(token, count)
-                    print('\n')
-
-                # 10,000件ごとに進捗を表示。
-                count_proccessed += 1
-                if count_proccessed % 10000 == 0:
-                    print('{0} documents were processed.'.format(count_proccessed),
-                          file=sys.stderr)
-
-    # 全記事の処理が完了したら、上位30件の名詞と出現回数を表示する。
-    #for token, count in frequency.most_common(30):
-        #print(token, count)
-
-
-def iter_docs(file):
-    """
-    ファイルオブジェクトを読み込んで、記事の中身（開始タグ <doc ...> と終了タグ </doc> の間のテキスト）を
-    順に返すジェネレーター関数。
-    """
-
-    for line in file:
-        if line.startswith('<doc '):
-            buffer = []  # 開始タグが見つかったらバッファを初期化する。
-            title = get_title_from_html(line)
-        elif line.startswith('</doc>'):
-            # 終了タグが見つかったらバッファの中身を結合してyieldする。
-            content = ''.join(buffer)
-            yield title, content
-        else:
-            buffer.append(line)  # 開始タグ・終了タグ以外の行はバッファに追加する。
-
-def get_title_from_html(line):
-    tokens = html.fromstring(line)
-    token = tokens.attrib['title']
-    return token
-
-def judge_person_name(tagger, name):
-    node = tagger.parseToNode(name)
-    while node:
-        category1, category2, category3 = node.feature.split(',')[:3]
-        if category1 == '名詞' and category2 == '固有名詞' and category3 == '人名':
-            print("------------", name, "------------")
-            return True
-        node = node.next
-    return False
-
-def get_tokens(tagger, content):
-    """
-    文書内に出現した名詞のリストを取得する関数。
-    """
-
-    tokens = []  # この記事で出現した名詞を格納するリスト。
-
-    node = tagger.parseToNode(content)
-    while node:
-        # node.featureはカンマで区切られた文字列なので、split()で分割して
-        # 最初の2項目をcategoryとsub_categoryに代入する。
-        category, sub_category = node.feature.split(',')[:2]
-        # 固有名詞または一般名詞の場合のみtokensに追加する。
-        if category == '名詞' and sub_category in ('固有名詞', '一般'):
-            tokens.append(node.surface)
-        node = node.next
-
-    return tokens
+    knowledge = CreateKnowledge(input_dir)
+    knowledge.Create()
 
 if __name__ == '__main__':
     main()
