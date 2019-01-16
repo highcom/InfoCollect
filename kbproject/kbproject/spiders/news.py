94037d96361c3ecf61106b773bc2b05596badcd1
晃一 <kouichi@kouichi-no-MacBook-Air.local>
2019-01-17 00:16:43 +0900

    CrawlSpiderを継承する修正

--- kbproject/kbproject/spiders/news.py
+++ kbproject/kbproject/spiders/news.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8 -*-
-import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 from kbproject.items import Headline
 
 
-class NewsSpider(scrapy.Spider):
+class NewsSpider(CrawlSpider):
     name = 'news'
     allowed_domains = ['news.yahoo.co.jp']
     start_urls = ['https://news.yahoo.co.jp/']
 
+    rules = (
+        Rule(LinkExtractor(allow=r'/pickup/\d+$'), callback='parse_topics'),
+    )
+    """
     def parse(self, response):
         #for url in response.css('ul.topics a::attr("href")').re(r'/pickup/¥d+$'):
         for url in response.css('ul.toptopics_list a::attr("href")').re(r'/pickup/\d+$'):
             yield scrapy.Request(response.urljoin(url), self.parse_topics)
+    """
     
     def parse_topics(self, response):
         item = Headline()
