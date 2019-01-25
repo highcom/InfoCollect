46731abb35e97d1a32196420dd3a9ba9894605ec
晃一 <kouichi@kouichi-no-MacBook-Air.local>
2019-01-26 00:06:07 +0900

    検索ワードのページをクロールする

--- kbproject/kbproject/spiders/news.py
+++ kbproject/kbproject/spiders/news.py
@@ -7,11 +7,13 @@ from kbproject.items import Headline
 
 class NewsSpider(CrawlSpider):
     name = 'news'
-    allowed_domains = ['news.yahoo.co.jp']
-    start_urls = ['https://news.yahoo.co.jp/']
+    allowed_domains = ['news.yahoo.co.jp', 'headlines.yahoo.co.jp']
+    #start_urls = ['https://news.yahoo.co.jp/']
+    start_urls = ['https://news.yahoo.co.jp/search/?p=阿部寛&ei=utf-8&fr=news_sw']
 
     rules = (
-        Rule(LinkExtractor(allow=r'/pickup/\d+$'), callback='parse_topics'),
+        #Rule(LinkExtractor(allow=r'/pickup/\d+$'), callback='parse_topics'),
+        Rule(LinkExtractor(allow=r'/article.+$'), callback='parse_topics'),
     )
     """
     def parse(self, response):
@@ -22,6 +24,9 @@ class NewsSpider(CrawlSpider):
     
     def parse_topics(self, response):
         item = Headline()
-        item['title'] = response.css('.newsTitle ::text').extract_first()
-        item['body'] = response.css('.hbody').xpath('string()').extract_first()
+        #item['title'] = response.css('.newsTitle ::text').extract_first()
+        #item['body'] = response.css('.hbody').xpath('string()').extract_first()
+        item['title'] = response.css('.hd').xpath('string()').extract_first()
+        item['body'] = response.css('.ynDetailText.yjDirectSLinkTarget').xpath('string()').extract_first()
+        
         yield item
